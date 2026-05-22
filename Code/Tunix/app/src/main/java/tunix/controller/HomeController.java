fbdb7d29b4cdaf31cc47938117e2a3c25d7cc4f8
theokarvoun <theokarvoun@gmail.com>
2026-05-22 14:12:49 +0300

    Worked on HomeView and Controller and LibraryView and Controller

--- Code/Tunix/app/src/main/java/tunix/controller/HomeController.java
+++ Code/Tunix/app/src/main/java/tunix/controller/HomeController.java
@@ -1,5 +1,15 @@
 package tunix.controller;
 
+import tunix.app.TunixApp;
+import tunix.view.center.HomeView;
+
 public class HomeController {
-    
+    HomeView view;
+    public HomeController(HomeView view) {
+        this.view = view;
+    }
+    public void draw() {
+        System.out.println("Drawing Home");
+        view.display();
+    }
 }
